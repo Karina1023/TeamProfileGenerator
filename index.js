06cc524d4837bba4b5eac0406baf17d651243701
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-04-27 16:07:50 -0500

    created folders and files and html pg

---
+++ index.js
@@ -0,0 +1,4 @@
+const Employee = require("./library/Employee");
+const {Manager, getManager} = require("./library/Manager");
+const {Engineer, getEngineer} = require("./library/Engineer");
+const { Intern, getIntern} = require("./library/Intern");
